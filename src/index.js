2ad65ff03117920846d1e6b50935caf47e2a3998
mainaliashish <mainaliashish@outlook.com>
2024-10-28 18:51:19 +0000

    update: - final refactor

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './components/StarRating'
 import reportWebVitals from './reportWebVitals';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return (
+    <div>
+      <StarRating color='blue' defaultRating={3} maxRating={10} onSetRating={setMovieRating}/>
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5}/>
+    <StarRating maxRating={5} defaultRating={3}/>
+    <StarRating size={25} color="red" defaultRating={4}/>
+    <StarRating size={32} color="green" defaultRating={2}/>
+    <Test/>
   </React.StrictMode>
 );
 
